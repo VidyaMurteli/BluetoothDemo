c1b9340acd16ab2b17b13e06d1ce9b2a404bca6c
vidya <vidyamurteli123@gmail.com>
2025-12-14 18:24:25 +0530

    Initial Commit

---
+++ BluetoothDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BluetoothDemo
+//
+//  Created by Saniha Hunur on 14/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
